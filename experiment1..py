3fc24cb684088e500849804c88a087689ff174fd
utkarshabarge680-sudo <utkarshabarge680@gmail.com>
2026-02-13 15:14:42 +0530

    Add files via upload

---
+++ experiment1..py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 09:14:15 2026
+
+@author: User
+"""
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+student_class = input("Enter your class; ")
+division = input("Enter your division: ")
+roll_no = input("Enter your roll number: ")
+batch = input("Enter your batch:  ")
+phone_no = input("Enter your phone number: ")
+blood_group = input("Enter your blood group: ")
+address = input("Enter your address: ")
+college = input("Enter your college name:")
+city = input("Enter your city: ")
+print("\n-----Student Information-----")
+print("Name:", name)
+print("Age:", age)
+print("Class:,student_class")
+print("Division:", division)
+print("Roll Number:", roll_no)
+print("Batch:", batch)
+print("Phone Number:", phone_no)
+print("Blood Group:", blood_group)
+print("Address:", address)
+print("College:", college)
+print("City:", city)
